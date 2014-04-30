b147358ef4b78377dd638212f3a2bbe06c85ad51
Garrett Rowell <garrettrowell@gmail.com>
2014-04-30 17:13:03 -0400

    some cleaning

--- src/CSET1200/conwayGOL.java
+++ src/CSET1200/conwayGOL.java
@@ -2,15 +2,54 @@ package CSET1200;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class conwayGOL {
-	
+
+	public Integer rows,cloumns;
+	public Integer getRows() {
+		return rows;
+	}
+	public void setRows(Integer rows) {
+		this.rows = rows;
+	}
+	public Integer getCloumns() {
+		return cloumns;
+	}
+	public void setCloumns(Integer cloumns) {
+		this.cloumns = cloumns;
+	}
+		
 	public static void main(String[] args) {
-		 
+		Integer gen=null;
+		BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("How many generations would you like to run?");
+		try {
+			gen=Integer.parseInt(buffer.readLine());
+		} 
+		catch (NumberFormatException e) {
+			e.printStackTrace();
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (buffer != null)buffer.close();
+			} 
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		System.out.println(gen);
+		firstGen();		
+	}
+	
+	public static void firstGen(){
+		conwayGOL cgol = new conwayGOL();
 		BufferedReader br = null;
 		String[] in = null;
-		Integer rows=0,columns=0;
 		int ints=0,lines=0;
 		String [][] first = null;
 		
@@ -26,20 +65,19 @@ public class conwayGOL {
 						if(in[i].matches("\\d"))
 						{
 							if(ints==0){
-								rows=Integer.parseInt(in[i]);
+								cgol.setRows(Integer.parseInt(in[i]));
 								ints++;
 							}
 							if(ints==1){
-								columns=Integer.parseInt(in[i]);
+								cgol.setCloumns(Integer.parseInt(in[i]));
 							}
 						}
 					}
 				}
 				
 				if (lines==1){
-					first = new String[rows][columns];
+					first = new String[cgol.getRows()][cgol.getCloumns()];
 				}
-				
 				if(lines>=1){
 					for (int i=0;i<in.length;i++){
 						if (!in[i].equals("")){
@@ -60,8 +98,8 @@ public class conwayGOL {
 		}
 		
 		//outputs first generation
-		for(int i=0;i<=rows-1;i++){
-			for(int j=0;j<=columns-1;j++){
+		for(int i=0;i<=cgol.getRows()-1;i++){
+			for(int j=0;j<=cgol.getCloumns()-1;j++){
 				System.out.println(i+" "+j+" "+first[i][j]);
 			}
 		}
